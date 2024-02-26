43a08880d3d1af93db47cbd5b27efdf9aaa3e35a
flashgrinder <flashgrinder92@gmail.com>
2024-02-26 19:24:22 +0400

    make default page

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+	<!-- News-detail -->
+	<section class="post js-reveal gs-reveal">
+		<article class="post__article">
+			<header class="post__header">
+				<div class="post__wrap container">
+					<h1 class="post__title title title--large title--black title--w-bold js-reveal gs-reveal a-reveal-left">
+						<?php the_title(); ?>
+					</h1>
+				</div>
+			</header>
+			<div class="post__body container">
+				<div class="post__wysiwyg wysiwyg">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</article>
+	</section>
+	<!-- ./ News-detail -->
+<?php get_footer(); ?>
