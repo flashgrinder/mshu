182bea74c2b83f73c40af7eef58a2acd004c7709
flashgrinder <flashgrinder92@gmail.com>
2024-04-16 12:16:55 +0400

    some change

--- front-page.php
+++ front-page.php
@@ -549,6 +549,7 @@
 
 <!-- Partners-info -->
 <section class="partners partners--info js-reveal gs-reveal">
+	<div style="display: none"></div>
 	<div class="partners__body container">
 		<h2 class="partners__heading title title--big title--red-gradient title--w-bold center title--indent-large js-reveal gs-reveal a-reveal-left">
 			Информационные партнёры
