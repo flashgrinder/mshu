846889d9ae9f112858325a109090622ada3a1e69
fcdigital <web@fcdigital.ru>
2025-01-21 12:49:04 +0400

    changed js for cookie message

--- src/libs/modules/cookie.js
+++ src/libs/modules/cookie.js
@@ -1,20 +1,17 @@
 function init() {
-    const acceptCookieBtn = document.querySelector('.cookie-accept-button');
-    const cookieModal = document.querySelector('.cookie-container');
-    
+
     if (localStorage.getItem('isCookieAccepted') !== 'true') {
-        console.log(localStorage.isCookieAccepted);
+
+        const acceptCookieBtn = document.querySelector('.cookie-accept-button');
+        const cookieModal = document.querySelector('.cookie-container');
+        
         cookieModal.classList.remove('hidden');
 
         acceptCookieBtn.addEventListener('click', () => {
             localStorage.setItem('isCookieAccepted', true);
             cookieModal.classList.add('hidden');
-            console.log(localStorage.getItem('isCookieAccepted'));
         })
     }
-    
-    
-    
 }
 
 export default { init }
